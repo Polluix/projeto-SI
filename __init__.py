8761b20b4a83e69609e1320a94e40a829f037399
Polluix <lipe311973@gmail.com>
2024-06-29 14:55:00 -0300

    add: preprocessamento da base com remoção de features desnecessárias

--- __init__.py
+++ __init__.py
@@ -4,10 +4,15 @@ from sklearn.tree import DecisionTreeClassifier as dtc
 from sklearn.neural_network import _multilayer_perceptron as mlp
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SequentialFeatureSelector as sfs
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
+import numpy as np
 
 
 # leitura da base
-base = pd.read_csv('0-Todos-CSV/25.csv')
+csv_name = '0-Todos-CSV/25.csv'
+
+base = pd.read_csv(csv_name)
 
 # numero de objetos de cada classe
 # print(base['0'].value_counts())
@@ -20,9 +25,50 @@ y = base.pop('0')
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=42,
                                                      stratify=y)
 
-# seleção de features para cada classificador utilizando
-# o sequential feature selection
+features = X_train.columns.to_list()
 
-sfs_knn = sfs(knn(n_neighbors=3))
-sfs_knn=sfs_knn.fit(X_train,y_train)
-print(sfs_knn.transform(X_train).shape())
+X_train = X_train.values
+y_train=y_train.values
+
+# -----------------SELECAO DE FEATURES--------------------
+n_estimator_list = [5,50,100,250,500,1000,1500,3000]
+X_train_new = []
+models = []
+for n in n_estimator_list:
+    clf = ExtraTreesClassifier(n_estimators=n)
+    clf = clf.fit(X_train, y_train)
+    feature_importances =clf.feature_importances_
+    # print(feature_importances_)
+
+    model = SelectFromModel(clf, prefit=True)
+    X_train_new.append(model.transform(X_train))
+    models.append(model)
+
+for base in X_train_new:
+    print(base.shape)
+
+# salvamento das bases de treino e teste após seleção de features
+
+selected_features = models[3].get_support()
+
+aux = []
+for i in range(len(features)):
+    if selected_features[i]:
+        aux.append(features[i])
+aux = np.array(aux)
+
+df = pd.DataFrame(X_train_new[3],columns=aux)
+df['CLASSE'] = y_train
+df.to_csv('./src/base_treino_preprocessada.csv',index=False)
+
+df2 = X_test
+df2['CLASSE'] = y_test
+df2.to_csv('./src/base_teste.csv',index=False)
+
+# ----------------------------------------------------------------
+
+# ----------------------
+
+# sfs_knn = sfs(knn(n_neighbors=3))
+# sfs_knn=sfs_knn.fit(X_train,y_train)
+# print(sfs_knn.transform(X_train).shape())
